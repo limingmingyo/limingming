2af4604fd23108cf5e04334c17ed25186fc14087
mingming <aoxyyd@163.com>
2022-10-26 09:35:52 +0800

    add lincense

--- mingming.js
+++ mingming.js
@@ -1,1 +1,17 @@
-console.log('limingming')
+// ==UserScript==
+// @name         超有用超厉害
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  赶紧用起来，很好用，很快!
+// @author       黎明明
+// @match        https://www.baidu.com/baidu?tn=monline_3_dg&ie=utf-8&wd=github
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=baidu.com
+// @license      AGPL-3.0
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
